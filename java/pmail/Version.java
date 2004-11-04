113d08af4a0d91f8c743d6cf2d32a44c552e9bf1
Andrea Odetti <mariofutire@googlemail.com>
2004-11-04 23:10:39 +0000

    New minor version. TODO: use labels!!!!!
    git-svn-id: https://andsoft.svn.sourceforge.net/svnroot/andsoft/trunk@23 4d501edd-990c-4f8e-b6a2-3c50872f52da

--- java/pmail/Version.java
+++ java/pmail/Version.java
@@ -1,6 +1,6 @@
 class Version {
     static private int major = 0 ;
-    static private int minor = 9 ;
-    static private int build = 83 ;
+    static private int minor = 11;
+    static private int build = 173 ;
     static public String ver = String.valueOf(major) + "." + String.valueOf(minor) + "." + String.valueOf(build);
 }
