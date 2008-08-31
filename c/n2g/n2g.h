32f782e23354bee4cb948523817e12472cdfef5a
Andrea Odetti <mariofutire@googlemail.com>
2008-08-31 07:49:01 +0000

    Update version of NL2SOL from Netlib.
    git-svn-id: https://andsoft.svn.sourceforge.net/svnroot/andsoft/trunk@71 4d501edd-990c-4f8e-b6a2-3c50872f52da

---
+++ c/n2g/n2g.h
@@ -0,0 +1,12 @@
+#include <f2c.h>
+
+extern "C" {
+/* Subroutine */ int dn2fb_(integer *n, integer *p, doublereal *x, doublereal 
+	*b, S_fp calcr, integer *iv, integer *liv, integer *lv, doublereal *v,
+			    integer *ui, doublereal *ur, U_fp uf);
+
+/* Subroutine */ int dn2f_(integer *n, integer *p, doublereal *x, S_fp calcr, 
+	integer *iv, integer *liv, integer *lv, doublereal *v, integer *
+			   uiparm, doublereal *urparm, U_fp ufparm);
+
+}
