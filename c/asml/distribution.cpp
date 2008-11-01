c7c99d5fa8614e5d9f422a4dde00231a09c6dd69
Andrea Odetti <mariofutire@googlemail.com>
2008-11-01 16:27:26 +0000

    New base class for distributions of the logarithm of the stock process. It will be used to price via characteristic function, with integration or FFT.
    git-svn-id: https://andsoft.svn.sourceforge.net/svnroot/andsoft/trunk@93 4d501edd-990c-4f8e-b6a2-3c50872f52da

---
+++ c/asml/distribution.cpp
@@ -0,0 +1,44 @@
+#include <asi/distribution.h>
+
+namespace ASI
+{
+    Distribution::~Distribution()
+    {
+    }
+
+    cpl Distribution::derivative(double x) const
+    {
+	const cpl x_down(x - 0.00001, 0.0);
+	const cpl x_up  (x + 0.00001, 0.0);
+
+	const cpl f_down = laplaceExponent(x_down);
+	const cpl f_up   = laplaceExponent(x_up);
+
+	const cpl value = (f_up - f_down) / (x_up - x_down);
+	return value;
+    }
+
+    BlackScholes::BlackScholes(double aSigmaSqrtT) : sigmaSqrtT(aSigmaSqrtT)
+    {
+    }
+	
+    cpl BlackScholes::laplaceExponent(const cpl & x) const
+    {
+	const cpl drift     = -0.5 * sigmaSqrtT * sigmaSqrtT * x;
+	const cpl diffusion = 0.5 * sigmaSqrtT * sigmaSqrtT * x * x;
+	return drift + diffusion;
+    }
+
+    double variance(const Distribution & dist)
+    {
+	const double mean   = exp(dist.laplaceExponent(1.0)).real();
+	const double second = exp(dist.laplaceExponent(2.0)).real();
+	const double var = second - mean * mean;
+	return var;
+    }
+
+    double varianceSwap(const Distribution & dist)
+    {
+	return -2.0 * dist.derivative(0.0).real();
+    }
+}
---
+++ c/asml/distribution.h
@@ -0,0 +1,38 @@
+#ifndef ASI_DISTRIBUTION_H
+#define ASI_DISTRIBUTION_H
+
+#include <asi/utils.h>
+
+namespace ASI
+{
+    class Distribution
+    {
+    public:
+	~Distribution();
+
+	/*
+	  E(e^(x * X)) = e^LE(x)
+	  x in C
+	  LE(0) = 0
+	  LE(1) = 1 when X is Log(S)
+	 */
+
+	virtual cpl laplaceExponent(const cpl & x) const = 0;
+	virtual cpl derivative(double x) const;
+    };
+
+    class BlackScholes : public Distribution
+    {
+    public:
+	BlackScholes(double aSigmaSqrtT);
+
+	virtual cpl laplaceExponent(const cpl & x) const;
+    private:
+	const double sigmaSqrtT;
+    };
+
+    double variance(const Distribution & dist);
+    double varianceSwap(const Distribution & dist);
+}
+
+#endif
