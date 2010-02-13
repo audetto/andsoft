37505e4a5c641412d10d44cc59a1227a07117c1b
Andrea Odetti <mariofutire@googlemail.com>
2010-02-13 21:49:17 +0000

    Added Copyright notice.
    The whole library/code is released underGPL 3.
    
    The code under "c" has not yet been given a Copyright notice.
    
    Jamaica is *not* copyrighted by AndSoft. It carries its own copyright notice.
    
    git-svn-id: https://andsoft.svn.sourceforge.net/svnroot/andsoft/trunk@203 4d501edd-990c-4f8e-b6a2-3c50872f52da

--- java/elves/asi/elves/Simulation.java
+++ java/elves/asi/elves/Simulation.java
@@ -1,3 +1,23 @@
+/*
+ *   Copyright 2004-2010 AndSoft Inc.  All Rights Reserved.
+ *
+ *   This file is part of the AndSoft Library.
+ *   http://www.odetti.it/andrea/index.htm
+ *
+ *   AndSoft is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   AndSoft is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with AndSoft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package asi.elves;
 
 import asi.elves.script.TimeSeries;
